af577c0c553a2326122ff504383643f8511b1913
U-EricsDell-PC\Erics <Erics@EricsDell-PC.(none)>
2014-07-23 09:16:16 -0400

    middle

--- static/KKscript.js
+++ static/KKscript.js
@@ -34,8 +34,8 @@ function changeLink() {
 function navig8() {
     localStorage.setItem("username", $('#enterfirstname')[0].value);
     localStorage.setItem("roomname", $('#enterroomname')[0].value);
-    console.log("username is......." + $('#enterfirstname')[0].value);
-    window.location.assign("/#createroom");
+    window.location.assign("#keyload");
+    //#window.location.assign("#createroom");
 }
 
 function formatKeyOutput(keystr) {
